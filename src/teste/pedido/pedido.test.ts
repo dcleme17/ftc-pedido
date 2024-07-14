d38018987fca139e3af2f327d3435a93c5869e74
Ciro Oshiro <ciroaoa@gmail.com>
2024-07-14 20:37:49 -0300

    testes apis... comentados

--- src/teste/pedido/pedido.test.ts
+++ src/teste/pedido/pedido.test.ts
@@ -1,11 +1,17 @@
 import { PedidoService } from '../repositorio/pedidoService';
 import { PedidoRepository } from '../repositorio/pedidoRepository';
 import { Pedido } from './pedido';
+//import { Cliente } from '../cliente/cliente';
+//import { ClienteVersao } from '../cliente/cliente.versao';
+//import { Pagamento } from '../pagamento/pagamento';
+//import { PagamentoVersao } from '../pagamento/pagamento.versao';
 import { PedidoVersao } from './pedido.versao';
 import { ItemPedido } from "../pedido/itemPedido";
-
-
+//import axios from 'axios';
 jest.mock('../repositorio/pedidoRepository');
+
+//jest.mock('axios');
+//const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('PedidoService', () => {
   let pedidoService: PedidoService;
@@ -62,5 +68,35 @@ describe('PedidoService', () => {
     expect(pedidoRepository.adiciona).toHaveBeenCalledWith(ped);
     expect(result).toBe(versao);
   });
+
+  // it('devePermitirObterClientePeloMetodoGet', async () => {
+  //   const cpf = '12345678909';
+  //   const cliente: Cliente = new Cliente("Nome Teste","12345678909","nome.teste@gmail.com", "1",new ClienteVersao("1", new Date()));
+  //   mockedAxios.get.mockResolvedValue({ data: cliente });
+
+  //   const result = await pedidoService.getClienteByCpf(cpf);
+  //   expect(result).toEqual(cliente);
+  //   expect(mockedAxios.get).toHaveBeenCalledWith(`/api/clientes/v1/${cpf}`);
+  // });
+
+  // it('devePermitirObterPagamentoPeloMetodoGet', async () => {
+  //   const codigoPedido = '12345678909';
+  //   const pagamento: Pagamento = new Pagamento("TESTE", "12345678909","teste@teste.com.br",1.99,1,"PIX","123456");
+  //   mockedAxios.get.mockResolvedValue({ data: pagamento });
+
+  //   const result = await pedidoService.getPagamentoByPedido(codigoPedido);
+  //   expect(result).toEqual(pagamento);
+  //   expect(mockedAxios.get).toHaveBeenCalledWith(`/api/pagamentos/v1/${codigoPedido}`);
+  // });
+
+  // it('devePermitircriarPagamentoPeloMetodoPost', async () => {
+  //   const pagamentoVersao = new PagamentoVersao("1",new Date());
+  //   const pagamento: Pagamento = new Pagamento("TESTE", "12345678909","teste@teste.com.br",1.99,1,"PIX","123456");
+  //   mockedAxios.get.mockResolvedValue({ data: pagamentoVersao });
+
+  //   const result = await pedidoService.createPagamento(pagamento);
+  //   expect(result).toEqual(pagamentoVersao);
+  //   expect(mockedAxios.post).toHaveBeenCalledWith('/api/clientes/v1', pagamento);
+  // });
 
 });
--- src/teste/repositorio/pedidoService.ts
+++ src/teste/repositorio/pedidoService.ts
@@ -3,6 +3,10 @@
   import { PedidoRepository } from './pedidoRepository';
   import { Pedido } from "../pedido/pedido";
   import { PedidoVersao } from "../pedido/pedido.versao";
+  import { Cliente } from "../cliente/cliente";
+  import { Pagamento } from "../pagamento/pagamento";
+  import { PagamentoVersao } from "../pagamento/pagamento.versao";
+  import axios from 'axios';
 
   export class PedidoService {
     private pedidoRepository: PedidoRepository;
@@ -20,6 +24,21 @@
         const ret = await this.pedidoRepository.adiciona(pedido);
       return ret;
     }
+
+    async getClienteByCpf(cpf: string): Promise<Cliente> {
+      const response = await axios.get(`/api/clientes/v1/${cpf}`);
+      return response.data;
+    }
+
+    async getPagamentoByPedido(codigoPedido: string): Promise<Pagamento> {
+      const response = await axios.get(`/api/pagamentos/v1/${codigoPedido}`);
+      return response.data;
+    }
   
+    async createPagamento(pagamento: Pagamento): Promise<PagamentoVersao> {
+      const response = await axios.post('/api/pagamentos/v1', pagamento);
+      return response.data;
+    }
+
   }
   
